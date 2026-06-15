0e36874a3dc455f4556aa1eee66b7f50f2fb901c
Rage4556 <xvrage@gmail.com>
2026-06-15 13:24:32 -0400

    Remove build scripts for Linux and PowerShell

--- build-linux.sh
+++
@@ -1,3 +0,0 @@
-export VINTAGE_STORY=/home/kyle/.config/VSLGameVersions/1.22.2
-dotnet restore Snowshoes.sln
-dotnet run --project ZZCakeBuild/CakeBuild.csproj -- --target Default --configuration Release
